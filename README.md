ed2542d9cfdb43bd6d08d14bdd7f4c52e1bd6b2e
Malik BEN KIRANE <free.malikbenkirane@gmail.com>
2018-04-27 13:36:47 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,19 @@
+screen.sh
+=========
+
+Usage
+-----
+Source Me.
+
+AUTHOR: Malik BEN KIRANE <FREE.MALIKBENKIRANE@GMAIL.COM>
+Licence: MIT (Licence)
+
+Summary:
+--------
+- no argument or just `ls`:
+  @ list all screens
+- one argument only:
+  @ starts a new session with this name if the session does not exist
+  @ reattach a session with this name
+- more than one argument:
+  @ normal behaviour
---
+++ screen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Source Me.
+#
+# AUTHOR: Malik BEN KIRANE <FREE.MALIKBENKIRANE@GMAIL.COM>
+# Licence: MIT (Licence)
+#
+# Read README.md to understand better
+
+screen() {
+    local args="$@"
+    if [ -z "$args" -o "$args"="ls" ]; then
+        command screen -ls
+    elif [ -z "$2" ]; then
+        if command screen -R "$1"; then
+            return 
+        else
+            command screen -S "$1"
+        fi
+    else
+        command screen $@
+    fi
+}
