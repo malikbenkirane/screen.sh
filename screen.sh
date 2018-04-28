f52e06b301749540f433b0778c464c6828f9d1ab
Malik BEN KIRANE <free.malikbenkirane@gmail.com>
2018-04-28 11:32:08 +0200

    Less verbose patch
    fix a bug when passing a non-existing screen

--- screen.sh
+++ screen.sh
@@ -11,11 +11,9 @@ screen() {
     if [ -z "$args" -o "$args" = "ls" ]; then
         command screen -ls
     elif [ -z "$2" ]; then
-        echo Tried to rattach screen..
         if command screen -rd "$1"; then
             return
         else
-            echo (fails) Invoked new screen
             command screen -S "$1"
         fi
     else
